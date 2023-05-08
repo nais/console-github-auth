8921b05d52ceecf49c4b85c89df4077026c8f4c2
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2023-05-08 11:00:25 +0200

    feat: let app start before GitHub app is installed

--- cmd/console-github-auth/main.go
+++ cmd/console-github-auth/main.go
@@ -22,6 +22,11 @@ var (
 	githubPrivateKeyPath = os.Getenv("GITHUB_PRIVATE_KEY_PATH")
 )
 
+func getAppInstallation(ctx context.Context, client *github.Client, organization string) (*github.Installation, error) {
+	appInstallation, _, err := client.Apps.FindOrganizationInstallation(ctx, organization)
+	return appInstallation, err
+}
+
 func main() {
 	ctx := context.Background()
 
@@ -45,15 +50,23 @@ func main() {
 
 	githubClient := github.NewClient(appClient)
 
-	appInstallation, _, err := githubClient.Apps.FindOrganizationInstallation(ctx, githubOrg)
+	appInstallation, err := getAppInstallation(ctx, githubClient, githubOrg)
 	if err != nil {
-		log.Errorf("could not find org installation: %v", err)
-		os.Exit(1)
+		log.Warnf("no GitHub installation found for org %v (yet). err: %v", githubOrg, err)
+	} else {
+		log.Infof("ready to serve tokens for installation: %v", appInstallation.GetID())
 	}
 
-	log.Infof("Serving tokens for installation: %v", appInstallation.GetID())
-
 	http.HandleFunc("/createInstallationToken", func(w http.ResponseWriter, r *http.Request) {
+		if appInstallation == nil {
+			appInstallation, err = getAppInstallation(ctx, githubClient, githubOrg)
+			if err != nil {
+				log.Warnf("no GitHub installation found for org %v - aborting token creation. err: %v", githubOrg, err)
+				fmt.Fprintf(w, "no GitHub installation found. Please install the nais/console app in your GitHub org.")
+				return
+			}
+		}
+
 		token, _, err := githubClient.Apps.CreateInstallationToken(r.Context(), appInstallation.GetID(), nil)
 		if err != nil {
 			fmt.Fprintf(w, "installation token error: %v", err)
